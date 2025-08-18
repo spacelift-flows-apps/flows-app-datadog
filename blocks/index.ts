12ff39ec88a273691cf4f5eb24956e3f3cd3e967
ilya-hontarau <ilyah@spacelift.io>
2025-08-18 16:37:00 +0200

    fix: index ts

--- blocks/index.ts
+++ blocks/index.ts
@@ -1,107 +1,36 @@
-/**
- * Generated Block Registry for DataDog API
- * 
- * This file is auto-generated. Do not edit manually.
- * Generated on: 2025-08-18T13:31:16.535Z
- * Total blocks: 45
- */
+// Import all blocks
+import { createMonitorV1 } from "./monitors/CreateMonitorV1";
+import { updateMonitorV1 } from "./monitors/UpdateMonitorV1";
+import { deleteMonitorV1 } from "./monitors/DeleteMonitorV1";
+import { getMonitorV1 } from "./monitors/GetMonitorV1";
+import { listMonitorsV1 } from "./monitors/ListMonitorsV1";
+import { createEventV2 } from "./events/CreateEventV2";
+import { getEventV2 } from "./events/GetEventV2";
+import { listEventsV2 } from "./events/ListEventsV2";
+import { searchEventsV2 } from "./events/SearchEventsV2";
 
-import { ListMonitorsV1 } from './ListMonitorsV1.js';
-import { CreateMonitorV1 } from './CreateMonitorV1.js';
-import { CheckCanDeleteMonitorV1 } from './CheckCanDeleteMonitorV1.js';
-import { SearchMonitorGroupsV1 } from './SearchMonitorGroupsV1.js';
-import { SearchMonitorsV1 } from './SearchMonitorsV1.js';
-import { ValidateMonitorV1 } from './ValidateMonitorV1.js';
-import { GetMonitorV1 } from './GetMonitorV1.js';
-import { UpdateMonitorV1 } from './UpdateMonitorV1.js';
-import { DeleteMonitorV1 } from './DeleteMonitorV1.js';
-import { ListMonitorDowntimesV1 } from './ListMonitorDowntimesV1.js';
-import { ValidateExistingMonitorV1 } from './ValidateExistingMonitorV1.js';
-import { ListAuditLogsV2 } from './ListAuditLogsV2.js';
-import { SearchAuditLogsV2 } from './SearchAuditLogsV2.js';
-import { ListCIAppPipelineEventsV2 } from './ListCIAppPipelineEventsV2.js';
-import { SearchCIAppPipelineEventsV2 } from './SearchCIAppPipelineEventsV2.js';
-import { ListCIAppTestEventsV2 } from './ListCIAppTestEventsV2.js';
-import { SearchCIAppTestEventsV2 } from './SearchCIAppTestEventsV2.js';
-import { ListEventsV2 } from './ListEventsV2.js';
-import { CreateEventV2 } from './CreateEventV2.js';
-import { SearchEventsV2 } from './SearchEventsV2.js';
-import { GetEventV2 } from './GetEventV2.js';
-import { ListLogsGetV2 } from './ListLogsGetV2.js';
-import { ListLogsV2 } from './ListLogsV2.js';
-import { GetMonitorNotificationRulesV2 } from './GetMonitorNotificationRulesV2.js';
-import { CreateMonitorNotificationRuleV2 } from './CreateMonitorNotificationRuleV2.js';
-import { GetMonitorNotificationRuleV2 } from './GetMonitorNotificationRuleV2.js';
-import { DeleteMonitorNotificationRuleV2 } from './DeleteMonitorNotificationRuleV2.js';
-import { UpdateMonitorNotificationRuleV2 } from './UpdateMonitorNotificationRuleV2.js';
-import { ListMonitorConfigPoliciesV2 } from './ListMonitorConfigPoliciesV2.js';
-import { CreateMonitorConfigPolicyV2 } from './CreateMonitorConfigPolicyV2.js';
-import { GetMonitorConfigPolicyV2 } from './GetMonitorConfigPolicyV2.js';
-import { DeleteMonitorConfigPolicyV2 } from './DeleteMonitorConfigPolicyV2.js';
-import { UpdateMonitorConfigPolicyV2 } from './UpdateMonitorConfigPolicyV2.js';
-import { ListMonitorUserTemplatesV2 } from './ListMonitorUserTemplatesV2.js';
-import { CreateMonitorUserTemplateV2 } from './CreateMonitorUserTemplateV2.js';
-import { ValidateMonitorUserTemplateV2 } from './ValidateMonitorUserTemplateV2.js';
-import { GetMonitorUserTemplateV2 } from './GetMonitorUserTemplateV2.js';
-import { UpdateMonitorUserTemplateV2 } from './UpdateMonitorUserTemplateV2.js';
-import { DeleteMonitorUserTemplateV2 } from './DeleteMonitorUserTemplateV2.js';
-import { ValidateExistingMonitorUserTemplateV2 } from './ValidateExistingMonitorUserTemplateV2.js';
-import { ListMonitorDowntimesV2 } from './ListMonitorDowntimesV2.js';
-import { ListRUMEventsV2 } from './ListRUMEventsV2.js';
-import { SearchRUMEventsV2 } from './SearchRUMEventsV2.js';
-import { ListSpansGetV2 } from './ListSpansGetV2.js';
-import { ListSpansV2 } from './ListSpansV2.js';
+// Re-export individual blocks
+export {
+  createMonitorV1,
+  updateMonitorV1,
+  deleteMonitorV1,
+  getMonitorV1,
+  listMonitorsV1,
+};
+export { createEventV2, getEventV2, listEventsV2, searchEventsV2 };
 
-/**
- * Dictionary of all available blocks
- */
+// Export all blocks as a dictionary
 export const blocks = {
-  ListMonitorsV1: ListMonitorsV1,
-  CreateMonitorV1: CreateMonitorV1,
-  CheckCanDeleteMonitorV1: CheckCanDeleteMonitorV1,
-  SearchMonitorGroupsV1: SearchMonitorGroupsV1,
-  SearchMonitorsV1: SearchMonitorsV1,
-  ValidateMonitorV1: ValidateMonitorV1,
-  GetMonitorV1: GetMonitorV1,
-  UpdateMonitorV1: UpdateMonitorV1,
-  DeleteMonitorV1: DeleteMonitorV1,
-  ListMonitorDowntimesV1: ListMonitorDowntimesV1,
-  ValidateExistingMonitorV1: ValidateExistingMonitorV1,
-  ListAuditLogsV2: ListAuditLogsV2,
-  SearchAuditLogsV2: SearchAuditLogsV2,
-  ListCIAppPipelineEventsV2: ListCIAppPipelineEventsV2,
-  SearchCIAppPipelineEventsV2: SearchCIAppPipelineEventsV2,
-  ListCIAppTestEventsV2: ListCIAppTestEventsV2,
-  SearchCIAppTestEventsV2: SearchCIAppTestEventsV2,
-  ListEventsV2: ListEventsV2,
-  CreateEventV2: CreateEventV2,
-  SearchEventsV2: SearchEventsV2,
-  GetEventV2: GetEventV2,
-  ListLogsGetV2: ListLogsGetV2,
-  ListLogsV2: ListLogsV2,
-  GetMonitorNotificationRulesV2: GetMonitorNotificationRulesV2,
-  CreateMonitorNotificationRuleV2: CreateMonitorNotificationRuleV2,
-  GetMonitorNotificationRuleV2: GetMonitorNotificationRuleV2,
-  DeleteMonitorNotificationRuleV2: DeleteMonitorNotificationRuleV2,
-  UpdateMonitorNotificationRuleV2: UpdateMonitorNotificationRuleV2,
-  ListMonitorConfigPoliciesV2: ListMonitorConfigPoliciesV2,
-  CreateMonitorConfigPolicyV2: CreateMonitorConfigPolicyV2,
-  GetMonitorConfigPolicyV2: GetMonitorConfigPolicyV2,
-  DeleteMonitorConfigPolicyV2: DeleteMonitorConfigPolicyV2,
-  UpdateMonitorConfigPolicyV2: UpdateMonitorConfigPolicyV2,
-  ListMonitorUserTemplatesV2: ListMonitorUserTemplatesV2,
-  CreateMonitorUserTemplateV2: CreateMonitorUserTemplateV2,
-  ValidateMonitorUserTemplateV2: ValidateMonitorUserTemplateV2,
-  GetMonitorUserTemplateV2: GetMonitorUserTemplateV2,
-  UpdateMonitorUserTemplateV2: UpdateMonitorUserTemplateV2,
-  DeleteMonitorUserTemplateV2: DeleteMonitorUserTemplateV2,
-  ValidateExistingMonitorUserTemplateV2: ValidateExistingMonitorUserTemplateV2,
-  ListMonitorDowntimesV2: ListMonitorDowntimesV2,
-  ListRUMEventsV2: ListRUMEventsV2,
-  SearchRUMEventsV2: SearchRUMEventsV2,
-  ListSpansGetV2: ListSpansGetV2,
-  ListSpansV2: ListSpansV2,
-} as const;
+  // Monitor V1
+  createMonitorV1,
+  updateMonitorV1,
+  deleteMonitorV1,
+  getMonitorV1,
+  listMonitorsV1,
 
-// Named exports for individual blocks
-export { ListMonitorsV1, CreateMonitorV1, CheckCanDeleteMonitorV1, SearchMonitorGroupsV1, SearchMonitorsV1, ValidateMonitorV1, GetMonitorV1, UpdateMonitorV1, DeleteMonitorV1, ListMonitorDowntimesV1, ValidateExistingMonitorV1, ListAuditLogsV2, SearchAuditLogsV2, ListCIAppPipelineEventsV2, SearchCIAppPipelineEventsV2, ListCIAppTestEventsV2, SearchCIAppTestEventsV2, ListEventsV2, CreateEventV2, SearchEventsV2, GetEventV2, ListLogsGetV2, ListLogsV2, GetMonitorNotificationRulesV2, CreateMonitorNotificationRuleV2, GetMonitorNotificationRuleV2, DeleteMonitorNotificationRuleV2, UpdateMonitorNotificationRuleV2, ListMonitorConfigPoliciesV2, CreateMonitorConfigPolicyV2, GetMonitorConfigPolicyV2, DeleteMonitorConfigPolicyV2, UpdateMonitorConfigPolicyV2, ListMonitorUserTemplatesV2, CreateMonitorUserTemplateV2, ValidateMonitorUserTemplateV2, GetMonitorUserTemplateV2, UpdateMonitorUserTemplateV2, DeleteMonitorUserTemplateV2, ValidateExistingMonitorUserTemplateV2, ListMonitorDowntimesV2, ListRUMEventsV2, SearchRUMEventsV2, ListSpansGetV2, ListSpansV2 };
+  // Events V2
+  createEventV2,
+  getEventV2,
+  listEventsV2,
+  searchEventsV2,
+} as const;
