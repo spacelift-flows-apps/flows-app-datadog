b113cdb997ee8805160972d0a125298c8c6e9364
ilya-hontarau <ilyah@spacelift.io>
2025-08-19 12:26:26 +0200

    feat: add dd api cfg validation

--- utils/defineDatadogBlock.ts
+++ utils/defineDatadogBlock.ts
@@ -150,7 +150,6 @@ export function defineDatadogBlock(params: DatadogBlockParams): AppBlock {
             headers["Content-Type"] = "application/json";
           }
 
-          console.log(requestBody);
           // Make the request
           const response = await fetch(url, {
             method,
